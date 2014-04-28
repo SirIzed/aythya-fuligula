7b4289468fb51710d40485ba3249b9be03f3e624
sir Ized <francois.brucker@centrale-marseille.fr>
2014-04-28 19:19:46 +0200

    first commit

---
+++ desktop/src/com/aythia/fuligula/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.aythia.fuligula.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.aythia.fuligula.AythiaFuligula;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AythiaFuligula(), config);
+	}
+}
---
+++ fripouille/src/main/java/com/aythia/fripouille/Grid.java
@@ -0,0 +1,11 @@
+/**
+ * Created on 28/04/2014.
+ */
+
+package com.aythia.fripouille;
+
+public class Grid {
+    public int answer() {
+        return 42;
+    }
+}
